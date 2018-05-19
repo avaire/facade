9b456cb98c8b6fb1f646e5ce1e95cc7948ecd108
Alexis Tan <alexis@sen-dev.com>
2018-05-19 17:48:46 +0200

    Copy configuration and logging from Ava + Add simple config file

--- src/main/java/com/avairebot/facade/Facade.java
+++ src/main/java/com/avairebot/facade/Facade.java
@@ -1,15 +1,38 @@
 package com.avairebot.facade;
 
+import com.avairebot.facade.config.Configuration;
+import com.avairebot.shared.ExitCodes;
 import org.apache.commons.cli.CommandLine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Facade {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Facade.class);
+
     private final CommandLine command;
+    private final Configuration config;
 
     public Facade(CommandLine command) {
         this.command = command;
 
-        System.out.println("Hello, World!");
+        System.out.println(getVersionInfo());
+
+        LOGGER.debug("====================================================");
+        LOGGER.debug("Starting the application with debug logging enabled!");
+        LOGGER.debug("====================================================\n");
+
+        LOGGER.info("Bootstrapping AvaIre Facade v" + AppInfo.getAppInfo().getVersion());
+
+        config = new Configuration(this, null, "facade.yml");
+        if (!config.exists()) {
+            LOGGER.info("The {} configuration file is missing!", "facade.yml");
+            LOGGER.info("Creating file and terminating program...");
+
+            config.saveDefaultConfig();
+
+            System.exit(ExitCodes.EXIT_CODE_NORMAL);
+        }
     }
 
     public static String getVersionInfo() {
@@ -24,5 +47,9 @@ public class Facade {
                 + "\n\tVersion:       " + AppInfo.getAppInfo().getVersion()
                 + "\n\tJVM:           " + System.getProperty("java.version")
                 + "\n";
+    }
+
+    public static Logger getLogger() {
+        return LOGGER;
     }
 }
