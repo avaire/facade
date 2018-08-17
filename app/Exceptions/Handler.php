aece3cb5d904c548a74cfcb77f4a0329d6fb7d70
Alexis Tan <alexis@sen-dev.com>
2018-08-17 14:31:40 +0200

    Add JSON response errors for 404 and 405 errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,11 +3,13 @@
 namespace App\Exceptions;
 
 use Exception;
-use Illuminate\Validation\ValidationException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -45,6 +47,18 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof NotFoundHttpException) {
+            return response([
+                'status' => 404,
+                'reason' => 'The requested route does not exists',
+            ], 404);
+        } else if ($exception instanceof MethodNotAllowedHttpException) {
+            return response([
+                'status' => 405,
+                'reason' => 'The requested resource does not allow the requested method',
+            ], 405);
+        }
+
         return parent::render($request, $exception);
     }
 }
